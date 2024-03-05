0d9e394975815345e4d10b910398ce06f2a4f8bd
VexScythe <davide.ludovici@yahoo.it>
2024-03-05 20:17:57 +0100

    fix column

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const container = document.querySelector(".container");
+const board = document.querySelector(".sketchboard");
 
 function makeGrid() {
     for (let i=0; i<16; i++){
+        const column = document.createElement('div');
+        column.classList.add('column');
+        column.textContent = `${i}`;
+        board.appendChild(column);
         for(let j=0; j<16; j++){
-            const div = document.createElement('div');
-            div.textContent = `${i}`;
-            div.setAttribute('style', 'display: flex; flex: 1; flex-direction: column;')
-            container.appendChild(div);
+           
         }
     }
 }
 
 makeGrid();
+
+/*
+            const div = document.createElement('div');
+            div.textContent = `${i}`;
+            div.setAttribute('style', 'display: flex; flex: 1; flex-direction: column;')
+            board.appendChild(div);
+
+*/
