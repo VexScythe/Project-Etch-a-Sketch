95103012d2a3388a3cab9b18d19ba83d84bb1d25
VexScythe <davide.ludovici@yahoo.it>
2024-03-05 22:51:28 +0100

    fix: function makeGrid now work properly

--- script.js
+++ script.js
@@ -4,20 +4,14 @@ function makeGrid() {
     for (let i=0; i<16; i++){
         const column = document.createElement('div');
         column.classList.add('column');
-        column.textContent = `${i}`;
         board.appendChild(column);
         for(let j=0; j<16; j++){
-           
+           const row = document.createElement('div');
+           row.classList.add('row');
+           row.textContent = `${j}`;
+           column.appendChild(row);
         }
     }
 }
 
 makeGrid();
-
-/*
-            const div = document.createElement('div');
-            div.textContent = `${i}`;
-            div.setAttribute('style', 'display: flex; flex: 1; flex-direction: column;')
-            board.appendChild(div);
-
-*/
