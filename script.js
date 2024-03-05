c76c987f6449b3d1a32bf65668631fe6e50673fb
VexScythe <davide.ludovici@yahoo.it>
2024-03-06 00:26:21 +0100

    feat: method to choose grid cells

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const board = document.querySelector(".sketchboard");
+const gridChoice = document.querySelector("#grid");
+const gridbtn = document.getElementById("gridbtn");
 
-function makeGrid() {
-    for (let i=0; i<16; i++){
+function makeGrid(cells) {
+    board.innerHTML='';
+    for (let i=0; i<cells; i++){
         const column = document.createElement('div');
         column.classList.add('column');
         board.appendChild(column);
-        for(let j=0; j<16; j++){
+        for(let j=0; j<cells; j++){
            const row = document.createElement('div');
            row.classList.add('row');
-           row.textContent = `${j}`;
            column.appendChild(row);
+           row.addEventListener('mouseover', () => row.setAttribute('style', 'background-color: black;'));
+       
+           
+           //row.addEventListener('mouseout', () => row.setAttribute('style', 'background-color: none;'))
         }
     }
 }
+console.log(gridChoice.value);
+gridbtn.addEventListener('click', (e) => makeGrid(gridChoice.value));
 
-makeGrid();
+/*function changeBackgroundColor(cell) {
+    this.setAttribute('style', 'background-color: black;');
+}*/
