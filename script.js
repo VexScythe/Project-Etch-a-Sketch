3f4cfda36e9a9eefa344782390a2883cac976c42
VexScythe <davide.ludovici@yahoo.it>
2024-03-06 02:07:19 +0100

    fix: grid cells input and connected errors

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const board = document.querySelector(".sketchboard");
 const gridChoice = document.querySelector("#grid");
 const gridbtn = document.getElementById("gridbtn");
+const gridInput = document.querySelector(".gridinput");
+let mousePressed = false;
+
+document.addEventListener("mousedown", () => mousePressed = true);
+document.addEventListener("mouseup", () => mousePressed = false);
+document.addEventListener("drag", () => mousePressed = false);
 
 function makeGrid(cells) {
     board.innerHTML='';
@@ -12,15 +18,29 @@ function makeGrid(cells) {
            const row = document.createElement('div');
            row.classList.add('row');
            column.appendChild(row);
-           row.addEventListener('mouseover', () => row.setAttribute('style', 'background-color: black;'));
+           row.addEventListener('mouseover', () => {
+           if (mousePressed){
+            row.setAttribute('style', 'background-color: black;');
+           } 
+        });
        
            
            //row.addEventListener('mouseout', () => row.setAttribute('style', 'background-color: none;'))
         }
     }
 }
-console.log(gridChoice.value);
-gridbtn.addEventListener('click', (e) => makeGrid(gridChoice.value));
+const error = document.createElement('div');
+gridbtn.addEventListener('click', (e) => {
+    error.classList.add('error');
+    gridInput.appendChild(error);
+    if (gridChoice.value >= 1 && gridChoice.value <= 100){
+        makeGrid(gridChoice.value);
+        gridChoice.value="";
+        error.innerText="";
+    }else{
+        error.textContent="Please submit a value from 1 to 100";
+    }
+});
 
 /*function changeBackgroundColor(cell) {
     this.setAttribute('style', 'background-color: black;');
